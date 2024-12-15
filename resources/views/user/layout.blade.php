52317713e44c8599b15c3234909aadd65e3899bd
DennisDon678 <dennisdon678@gmail.com>
2024-12-15 00:46:11 -0600

    smartsupp

--- resources/views/user/layout.blade.php
+++ resources/views/user/layout.blade.php
@@ -35,7 +35,7 @@
             <ion-refresher id="refresher" slot="fixed">
                 <ion-refresher-content></ion-refresher-content>
             </ion-refresher>
-           
+
             @yield('content')
         </ion-content>
 
@@ -99,17 +99,37 @@
         });
     </script>
 
-<script>
-    window.gtranslateSettings = {
-        "default_language": "en",
-        "native_language_names": true,
-        "detect_browser_language": true,
-        "wrapper_selector": ".gtranslate_wrapper"
-    }
-</script>
-<script src="https://cdn.gtranslate.net/widgets/latest/dropdown.js" defer></script>
+    <script>
+        window.gtranslateSettings = {
+            "default_language": "en",
+            "native_language_names": true,
+            "detect_browser_language": true,
+            "wrapper_selector": ".gtranslate_wrapper"
+        }
+    </script>
+    <script src="https://cdn.gtranslate.net/widgets/latest/dropdown.js" defer></script>
 
     @yield('script')
+    <script type="text/javascript">
+        var _smartsupp = _smartsupp || {};
+        _smartsupp.key = 'c021638ce5e4020a0eda01591ba316fb8bf7dd69';
+        _smartsupp.offsetY = 100;
+        window.smartsupp || (function(d) {
+            var s, c, o = smartsupp = function() {
+                o._.push(arguments)
+            };
+            o._ = [];
+            s = d.getElementsByTagName('script')[0];
+            c = d.createElement('script');
+            c.type = 'text/javascript';
+            c.charset = 'utf-8';
+            c.async = true;
+            c.src = 'https://www.smartsuppchat.com/loader.js?';
+            s.parentNode.insertBefore(c, s);
+        })(document);
+    </script>
+    <noscript> Powered by <a href=“https://www.smartsupp.com” target=“_blank”>Smartsupp</a></noscript>
+
 </body>
 
 </html>
